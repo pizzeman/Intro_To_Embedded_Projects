ff3d5f0e546bce1ce8c188358bd8ea9a0bf8f1fd
Isaiah <ijbum2@gmail.com>
2025-11-10 13:24:00 -0500

    Establishing initial files

--- MKII_ToggleMuticolorLED/Debug/LP_MSPM0G3507/mspm0g350x_int.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-LP_MSPM0G3507/mspm0g350x_int.o: ../LP_MSPM0G3507/mspm0g350x_int.c \
- ../LP_MSPM0G3507/mspm0g350x_int.h
-../LP_MSPM0G3507/mspm0g350x_int.h:
--- MKII_ToggleMuticolorLED/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-l"C:/TI/mspm0_sdk_2_07_00_05/source/ti/driverlib/lib/ticlang/m0p/mspm0g1x0x_g3x0x/driverlib.a" -Wl,-llibc.a
-
