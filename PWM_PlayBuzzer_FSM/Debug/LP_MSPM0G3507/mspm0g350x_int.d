9ddd8f5a2b5358c624a992c4e736b51bc7bf2871
pizzeman <68896538+pizzeman@users.noreply.github.com>
2025-11-10 14:18:54 -0500

    Trying to Link Folders Using Relative Path

--- PWM_PlayBuzzer_FSM/Debug/LP_MSPM0G3507/mspm0g350x_int.d
+++ PWM_PlayBuzzer_FSM/Debug/LP_MSPM0G3507/mspm0g350x_int.d
@@ -1,6 +1,6 @@
 # FIXED
 
 LP_MSPM0G3507/mspm0g350x_int.o: \
- C:/Users/perid/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.c \
- C:/Users/perid/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.h
-C:/Users/perid/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.h:
+ C:/Users/amosa/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.c \
+ C:/Users/amosa/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.h
+C:/Users/amosa/Intro_To_Embedded_Projects/LP_MSPM0G3507/mspm0g350x_int.h:
